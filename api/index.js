d98ac874fc1454dd28c8b17fc9367f798208eb9d
Ignacio Nicolás Fuentes <infuentes96@gmail.com>
2022-10-03 22:56:31 -0300

    bugs

--- api/index.js
+++ api/index.js
@@ -22,9 +22,9 @@ const { conn } = require("./src/db.js");
 const { createGenres } = require("./src/controllers/genres.js");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then( async () => {
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
-    createGenres();
   });
+  await createGenres();
 })
